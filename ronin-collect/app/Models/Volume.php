cf1ef635960d26d17a6f614da379f0d44e7ddbc6
arlodel <nurrahman1505@gmail.com>
2026-04-13 08:44:19 +0700

    Memperbaiki error format tanggal pada halaman show dengan menambahkan tipe cast ke Model Volume

--- ronin-collect/app/Models/Volume.php
+++ ronin-collect/app/Models/Volume.php
@@ -8,6 +8,10 @@ class Volume extends Model
 {
     protected $guarded = [];
 
+    protected $casts = [
+        'acquisition_date' => 'date',
+    ];
+
     public function comic()
     {
         return $this->belongsTo(Comic::class);
